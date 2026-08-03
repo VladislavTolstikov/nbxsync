6e88296804dbe7a6602b6c361deebcda9867e5fb
VladislavTolstikov <102664327+VladislavTolstikov@users.noreply.github.com>
2026-08-03 10:21:03 +0300

    Use Site Group hierarchy for NbxSync site resolution

--- nbxsync/services/__init__.py
+++ nbxsync/services/__init__.py
@@ -1,3 +1,10 @@
-from .autofill import AutofillError, AutofillResult, fill_nbxsync_device
+from . import autofill as _autofill
+from .site_resolution import resolve_site_key
+
+_autofill._site_key = resolve_site_key
+
+AutofillError = _autofill.AutofillError
+AutofillResult = _autofill.AutofillResult
+fill_nbxsync_device = _autofill.fill_nbxsync_device
 
 __all__ = ('AutofillError', 'AutofillResult', 'fill_nbxsync_device')
