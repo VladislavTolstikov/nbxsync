df66a09b18fc2207341f5e2e4f986ade6658c8e7
VladislavTolstikov <102664327+VladislavTolstikov@users.noreply.github.com>
2026-08-10 17:38:11 +0300

    Continue host deletion across all assigned Zabbix servers

--- nbxsync/jobs/deletehost.py
+++ nbxsync/jobs/deletehost.py
@@ -9,14 +9,30 @@ __all__ = ('DeleteHostJob',)
 
 class DeleteHostJob:
     def __init__(self, **kwargs):
-        self.instance = kwargs.get('instance')  # This is the Device or VirtualMachine object
+        self.instance = kwargs.get('instance')  # Device or VirtualMachine
 
     def run(self):
         object_ct = ContentType.objects.get_for_model(self.instance)
-        zabbixserver_assignments = ZabbixServerAssignment.objects.filter(assigned_object_type=object_ct, assigned_object_id=self.instance.pk)
+        zabbixserver_assignments = ZabbixServerAssignment.objects.filter(
+            assigned_object_type=object_ct,
+            assigned_object_id=self.instance.pk,
+        )
 
+        errors = []
         for assignment in zabbixserver_assignments:
-            self.delete_host(assignment)
+            try:
+                self.delete_host(assignment)
+            except Exception as error:
+                errors.append(
+                    f'assignment={assignment.pk} '
+                    f'server={assignment.zabbixserver_id}: {error}'
+                )
+
+        if errors:
+            raise RuntimeError(
+                'Host deletion completed with errors on one or more Zabbix '
+                'servers: ' + ' | '.join(errors)
+            )
 
     def delete_host(self, assignment):
         safe_delete(HostSync, assignment)
