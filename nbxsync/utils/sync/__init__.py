2e03ca0a72e4518e87f6807c0e0a2d4b72b217af
VladislavTolstikov <102664327+VladislavTolstikov@users.noreply.github.com>
2026-08-10 17:38:36 +0300

    Install HostSync lifecycle policy guard

--- nbxsync/utils/sync/__init__.py
+++ nbxsync/utils/sync/__init__.py
@@ -6,8 +6,10 @@ from .hostinterfacesync import HostInterfaceSync
 from .maintenancesync import MaintenanceSync
 from .run_zabbix_operations import run_zabbix_operation
 from .host_delete import install as install_host_delete
+from .host_policy_guard import install as install_host_policy_guard
 from .identity_guard import install as install_identity_guard
 
 
 install_host_delete(HostSync)
+install_host_policy_guard(HostSync)
 install_identity_guard(HostSync, HostInterfaceSync)
