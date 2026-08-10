59c8d812d6dbc997d1ccb9c7295fa43e62e4c6d2
VladislavTolstikov <102664327+VladislavTolstikov@users.noreply.github.com>
2026-08-10 16:57:13 +0300

    Install safe host delete runtime patch

--- nbxsync/utils/sync/__init__.py
+++ nbxsync/utils/sync/__init__.py
@@ -5,7 +5,9 @@ from .hostgroupsync import HostGroupSync
 from .hostinterfacesync import HostInterfaceSync
 from .maintenancesync import MaintenanceSync
 from .run_zabbix_operations import run_zabbix_operation
+from .host_delete import install as install_host_delete
 from .identity_guard import install as install_identity_guard
 
 
+install_host_delete(HostSync)
 install_identity_guard(HostSync, HostInterfaceSync)
