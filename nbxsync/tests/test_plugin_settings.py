59d617b8d099bdb68601932ae1b3b6c87e1e0917
VladislavTolstikov <102664327+VladislavTolstikov@users.noreply.github.com>
2026-09-23 09:11:46 +0300

    Test NetBox host link plugin settings

--- nbxsync/tests/test_plugin_settings.py
+++ nbxsync/tests/test_plugin_settings.py
@@ -4,7 +4,13 @@ from django.test import TestCase
 from pydantic import ValidationError
 
 from nbxsync.choices.syncsot import SyncSOT
-from nbxsync.settings import PluginSettingsModel, SNMPConfig, BackgroundSyncConfig, get_plugin_settings
+from nbxsync.settings import (
+    BackgroundSyncConfig,
+    NetBoxLinkConfig,
+    PluginSettingsModel,
+    SNMPConfig,
+    get_plugin_settings,
+)
 
 
 class PluginSettingsModelTestCase(TestCase):
@@ -15,6 +21,10 @@ class PluginSettingsModelTestCase(TestCase):
         self.assertIsInstance(settings.statusmapping.device, dict)
         self.assertIsInstance(settings.statusmapping.virtualmachine, dict)
         self.assertIsInstance(settings.snmpconfig, SNMPConfig)
+        self.assertIsInstance(settings.netbox_link, NetBoxLinkConfig)
+        self.assertFalse(settings.netbox_link.enabled)
+        self.assertEqual(settings.netbox_link.macro, '{$NETBOX.URL}')
+        self.assertIsNone(settings.netbox_link.base_url)
         self.assertIsInstance(settings.backgroundsync.objects, BackgroundSyncConfig)
         self.assertIsInstance(settings.backgroundsync.templates, BackgroundSyncConfig)
         self.assertIsInstance(settings.backgroundsync.proxies, BackgroundSyncConfig)
@@ -46,3 +56,13 @@ class PluginSettingsModelTestCase(TestCase):
 
         settings = get_plugin_settings()
         self.assertIsInstance(settings, PluginSettingsModel)
+
+
+class NetBoxLinkConfigTestCase(TestCase):
+    def test_base_url_is_normalized(self):
+        config = NetBoxLinkConfig(enabled=True, base_url='https://nbx.muctr.ru/')
+        self.assertEqual(config.base_url, 'https://nbx.muctr.ru')
+
+    def test_invalid_macro_is_rejected(self):
+        with self.assertRaises(ValidationError):
+            NetBoxLinkConfig(macro='NETBOX.URL')
